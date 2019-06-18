4d2564fac0116307f1aa2fe8e8b62f38aa92458e
vallurna <NagaSarathChand.Valluri@arhs-spikeseed.com>
2019-06-18 21:57:00 +0200

    fix for static file path after build

--- public/server/express-app.js
+++ public/server/express-app.js
@@ -8,7 +8,10 @@ let PasswordService =  require('./password-service').PasswordService;
 
 app.use(bodyParser.json());
 app.use(cors({credentials: true, origin: true}));
-app.use(express.static('build'));
+console.log(require('path').dirname(require.main.filename));
+app.use(express.static(require('path').dirname(require.main.filename)));
+//use this wen you run the target electron . when you run without pack
+// app.use(express.static('./build'));
 
 new PasswordService(app);
 app.listen(port, ()=> console.log('express app on port ${port} is up and running !!!'));
